8f198569e55999de4cd3010ffd2a8c4a76a1ca29
sasano yasuhiro <sasanoworkz@gmail.com>
2020-09-06 18:00:17 +0900

    use csv reader

--- src/view/csv_uploader.js
+++ src/view/csv_uploader.js
@@ -6,12 +6,44 @@ import './csv_uploader.scss'
 export default class CsvUploader extends React.Component{
   constructor(props){
     super(props)
+    this.state = {
+      encode: 'sjis',
+      csv_data: [],
+    }
+    this.onFileLoaded = this.onFileLoaded.bind(this)
+    this.format = [
+      { value: 'sjis',  label: 'sjis'  },
+      { value: 'utf-8',  label: 'utf-8'  },
+    ];
+  }
+  onFileLoaded(data){
+    this.setState({csv_data: data})
+  }
+  onSelect(e){
+    this.setState({encode: e.target.value})
   }
   render(){
     return(
       <div className='csv_uploader'>
-        <CSVReader/>
+        {this.render_encoding()}
+        <CSVReader
+          onFileLoaded={this.onFileLoaded}
+          fileEncoding={this.state.encode}
+        />
+        {}
       </div>
+    )
+  }
+  render_encoding(){
+    return(
+      <select
+        defaultValue={this.state.encode}
+        onChange={this.onSelect.bind(this)}
+      >
+        {this.format.map( d =>
+          <option key={d.value} value={d.value}>{d.label}</option>
+        )}
+      </select>
     )
   }
 }
