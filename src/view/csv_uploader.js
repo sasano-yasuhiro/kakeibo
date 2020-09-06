d7b0265072235e5a6aae69ab4b0212d81547c7f8
sasano yasuhiro <sasanoworkz@gmail.com>
2020-09-06 22:36:03 +0900

    見出し選択の表示部分

--- src/view/csv_uploader.js
+++ src/view/csv_uploader.js
@@ -9,6 +9,7 @@ export default class CsvUploader extends React.Component{
     this.state = {
       encode: 'sjis',
       csv_data: [],
+      title: [],
     }
     this.onFileLoaded = this.onFileLoaded.bind(this)
     this.format = [
@@ -17,10 +18,27 @@ export default class CsvUploader extends React.Component{
     ];
   }
   onFileLoaded(data){
-    this.setState({csv_data: data})
+    let title= [];
+    for(let i=0; i < data[0].length; i++){
+      title.push({check:false, value:""})
+    }
+    this.setState({
+      csv_data: data,
+      title: title,
+    })
   }
   onSelect(e){
     this.setState({encode: e.target.value})
+  }
+  toggleCheck(e){
+    let title= this.state.title
+    title[e.target.value].check = !title[e.target.value].check 
+    this.setState({title: title})
+  }
+  onChange(e){
+    let title= this.state.title
+    title[e.target.id].value = e.target.value
+    this.setState({title: title})
   }
   render(){
     return(
@@ -30,7 +48,8 @@ export default class CsvUploader extends React.Component{
           onFileLoaded={this.onFileLoaded}
           fileEncoding={this.state.encode}
         />
-        {}
+        {this.render_row_one()}
+        {this.render_csv_table()}
       </div>
     )
   }
@@ -44,6 +63,40 @@ export default class CsvUploader extends React.Component{
           <option key={d.value} value={d.value}>{d.label}</option>
         )}
       </select>
+    )
+  }
+  render_row_one(){
+    let csv_data = this.state.csv_data
+    return(
+      <div>
+        {csv_data.length > 0 &&
+          csv_data[0].map( (d, i) =>
+           <div key={'row_one_'+i} hidden={d?false:true}>
+             <input
+               type="checkbox"
+               defaultValue={i}
+               checked={this.state.title[i].check}
+               onChange={this.toggleCheck.bind(this)}
+             />
+             <span>{d}</span>
+             <input
+               id={i}
+               type='text'
+               defaultValue={this.state.title[i].value}
+               onChange={this.onChange.bind(this)}
+             />
+           </div>
+        )}
+      </div>
+    )
+  }
+  render_csv_table(){
+    return(
+      <table>
+        <tbody>
+          <tr></tr>
+        </tbody>
+      </table>
     )
   }
 }
